2143fffde764b29b4ac04d3b302feab60c367fb4
Codrut Befu <codrut_befu@yahoo.com>
2026-08-22 22:16:39 +0300

    note

--- resources/views/services/payment-note.blade.php
+++ resources/views/services/payment-note.blade.php
@@ -9,62 +9,97 @@
             margin: 0;
         }
 
+        html {
+            margin:10px;
+        }
+
         * {
             box-sizing: border-box;
         }
 
         body {
             margin: 0;
-            background: #eee;
+            background: #fff;
             color: #111;
             font-family: Arial, Helvetica, sans-serif;
+            font-size: 9pt;
         }
 
         .page {
-            width: 210mm;
-            min-height: 297mm;
-            margin: 0 auto;
-            background: #fff;
-            padding: 25mm 15mm 12mm;
-        }
-
-        .copy {
             width: 100%;
-            min-height: 260mm;
             position: relative;
+            margin-top:20px;
         }
 
-        .vertical-no,
-        .vertical-id {
+        .document {
+            min-height: 261mm;
+            position: relative;
+            width: 50%;
+        }
+
+        .document > div {
+            padding: 10px;
+        }
+
+        .document:nth-child(1) {
+            margin-right:5px;
+            border-right:1px dashed #111;
+            float: left;
+
+        }
+
+        .document:nth-child(2){
+            float: right;
+            margin-left:5px;
+        }
+        
+        .vertical {
             position: absolute;
-            right: 0;
+            right: 10px;
+            font-family: "Times New Roman", Times, serif;
+            font-size: 8pt;
+            font-weight: bold;
+            line-height: 1;
             writing-mode: vertical-rl;
-            font: bold 8pt "Times New Roman", serif;
+            text-align: left;
+              white-space: nowrap;
+            transform: rotate(90deg);
+            transform-origin: top right;
         }
 
-        .vertical-no {
+        .clearfix {
+            clear: both;
+        }
+        .vertical.no {
             top: 0;
         }
 
-        .vertical-id {
+        .vertical.number {
             top: 18mm;
         }
 
-        h1 {
-            font-size: 16pt;
-            text-align: center;
+        .title {
             margin: 0 0 5mm;
-            font-weight: 700;
+            text-align: center;
+            font-size: 16pt;
+            font-weight: bold;
+            letter-spacing: .2pt;
         }
 
         .meta {
-            font-size: 9pt;
-            line-height: 1.45;
             margin-left: 4mm;
+            line-height: 1.42;
         }
 
-        .meta strong.name {
+        .meta .code {
+            font-weight: bold;
+        }
+
+        .meta .name {
+            display: block;
+            margin-top: 1mm;
             font-size: 12pt;
+            font-weight: bold;
             text-transform: uppercase;
         }
 
@@ -72,65 +107,102 @@
             font-style: italic;
         }
 
-        .bolditalic {
-            font-weight: 700;
+        .bold-italic {
             font-style: italic;
+            font-weight: bold;
         }
 
         .items {
+            width: 100%;
             margin-top: 6mm;
+            border-collapse: collapse;
             border-top: 2px solid #111;
-            font-family: "Times New Roman", serif;
+            font-family: "Times New Roman", Times, serif;
             font-size: 8pt;
         }
 
-        .itemrow {
-            display: grid;
-            grid-template-columns: 8mm 1fr 12mm 25mm 25mm;
+        .items td {
+            height: 7mm;
+            padding: 1mm 1.2mm;
             background: #efefef;
-            align-items: center;
-            min-height: 7mm;
+            vertical-align: middle;
         }
 
-        .itemrow div {
-            padding: 1mm;
+        .vat {
+            width: 8mm;
         }
 
-        .right {
+        .description {
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;
+        }
+
+        .qty {
+            width: 12mm;
             text-align: right;
+            font-weight: bold;
         }
 
-        .desc {
-            font-weight: 700;
-            font-family: Arial, sans-serif;
+        .price,
+        .line-total {
+            width: 25mm;
+            text-align: right;
+            font-weight: bold;
         }
 
-        .benef {
-            display: grid;
-            grid-template-columns: 22mm 1fr 14mm 1fr;
+        .beneficiary {
+            width: 100%;
+            margin-top: 2mm;
+            border-collapse: collapse;
             font-size: 7.5pt;
-            font-style: italic;
-            padding: 2mm 0 0;
         }
 
-        .benef .plain {
+        .beneficiary td {
+            padding: 0;
+            vertical-align: top;
+        }
+
+        .beneficiary .label {
+            width: 22mm;
+            font-style: italic;
+        }
+
+        .beneficiary .plain {
             font-style: normal;
         }
 
-        .activation {
-            font-size: 7.4pt;
+        .beneficiary .card-label {
+            width: 14mm;
             font-style: italic;
-            margin: 2mm 0 8mm;
         }
 
-        .total {
+        .activation {
+            margin-top: 2mm;
+            margin-bottom: 8mm;
+            font-size: 7.4pt;
+            font-style: italic;
+        }
+
+        .total-table {
+            width: 100%;
+            border-collapse: collapse;
             border-top: 2px solid #111;
-            padding-top: 4mm;
-            display: flex;
-            justify-content: flex-end;
-            gap: 22mm;
-            font-weight: 700;
             font-size: 11pt;
+            font-weight: bold;
+        }
+
+        .total-table td {
+            padding-top: 4mm;
+        }
+
+        .total-label {
+            text-align: right;
+            padding-right: 22mm;
+        }
+
+        .total-amount {
+            width: 30mm;
+            text-align: right;
         }
 
         .order {
@@ -148,81 +220,85 @@
         }
 
         .company {
-            font-weight: 700;
-            font-style: italic;
-            font-size: 10pt;
             margin-left: 3mm;
+            font-size: 10pt;
+            font-style: italic;
+            font-weight: bold;
         }
 
-        .addr {
+        .address {
+            margin: 1mm 0 0 5mm;
             font-size: 7.5pt;
             line-height: 1.45;
-            margin: 1mm 0 0 5mm;
         }
 
-        .code {
-            font: italic 6.5pt "Times New Roman", serif;
+        .document-code {
             margin: 8mm 0 1mm 5mm;
+            font-family: "Times New Roman", Times, serif;
+            font-size: 6.5pt;
+            font-style: italic;
         }
 
         .software {
             margin-left: 5mm;
-            font: 7pt "Times New Roman", serif;
-        }
-
-        @media print {
-            body {
-                background: #fff;
-            }
-
-            .page {
-                margin: 0;
-            }
+            font-family: "Times New Roman", Times, serif;
+            font-size: 7pt;
         }
     </style>
 </head>
 <body>
     <div class="page">
-        <section class="copy">
-            <div class="vertical-no">No.</div>
-            <div class="vertical-id">{{ $noteNumber }}</div>
+        @foreach ([1, 2] as $copyIndex)
+        <section class="document">
+            <div>
+            <div class="vertical no">No.</div>
+            <div class="vertical number">{{ $noteNumber }}</div>
 
-            <h1>NOTA DE PLATA</h1>
+            <h1 class="title">NOTA DE PLATA</h1>
 
             <div class="meta">
                 {{ $issuedAt->format('d-M-Y') }}&nbsp;&nbsp;{{ $issuedAt->format('H:i') }}<br><br>
-                <strong>SPA ID: {{ $user->user_code ?: $user->id }}</strong><br>
-                <strong class="name">{{ $fullName }}</strong><br>
+                <span class="code">SPA ID: {{ $user->user_code ?: $user->id }}</span><br>
+                <span class="name">{{ $fullName }}</span>
                 {{ $user->email ?: '-' }}<br><br>
                 <span class="italic">Nota plata service</span><br>
-                <span class="bolditalic">Extern</span><br>
+                <span class="bold-italic">Extern</span><br>
                 <span class="italic">Independent</span>
             </div>
 
-            <div class="items">
-                <div class="itemrow">
-                    <div>21%</div>
-                    <div class="desc">{{ $service->name }}</div>
-                    <div class="right"><b>1.00</b></div>
-                    <div class="right"><b>{{ $amount }}</b></div>
-                    <div class="right"><b>{{ $amount }}</b></div>
-                </div>
-            </div>
+            <table class="items">
+                <tr>
+                    <td class="vat">21%</td>
+                    <td class="description">{{ $service->name }}</td>
+                    <td class="qty">1.00</td>
+                    <td class="price">{{ $amount }}</td>
+                    <td class="line-total">{{ $amount }}</td>
+                </tr>
+            </table>
 
-            <div class="benef">
-                <div>Beneficiar</div>
-                <div class="plain">{{ $fullName }}</div>
-                <div>Card:</div>
-                <div class="plain">{{ $cardCode }}&nbsp; Data Activ : {{ $startDate }}</div>
-            </div>
+            <table class="beneficiary">
+                <tr>
+                    <td class="label">Beneficiar</td>
+                    <td class="plain">{{ $fullName }}</td>
+                    <td class="card-label">Card:</td>
+                    <td class="plain">{{ $cardCode }}&nbsp; Data Activ : {{ $startDate }}</td>
+                </tr>
+            </table>
 
             <div class="activation">activare card</div>
-            <div class="total"><span>TOTAL PLATA:</span><span>{{ $amount }}</span></div>
+
+            <table class="total-table">
+                <tr>
+                    <td class="total-label">TOTAL PLATA:</td>
+                    <td class="total-amount">{{ $amount }}</td>
+                </tr>
+            </table>
+
             <div class="order">{{ $orderDetails }}</div>
 
             <div class="footer">
                 <div class="company">{{ $organization?->name ?? 'Organizatie' }}</div>
-                <div class="addr">
+                <div class="address">
                     {{ $organization?->address ?? '-' }}<br>
                     Email: {{ $organization?->email ?? '-' }}&nbsp;&nbsp; Web: {{ $organization?->web ?? '-' }}<br>
                     Tel: {{ $organization?->phone ?? '-' }}<br><br>
@@ -232,10 +308,13 @@
                     Cont: {{ $organization?->cont ?? '-' }}<br>
                     Banca: {{ $organization?->bank ?? '-' }}
                 </div>
-                <div class="code">{{ $assignment->bill_number ?: $noteNumber }}</div>
+                <div class="document-code">{{ $assignment->bill_number ?: $noteNumber }}</div>
                 <div class="software">Document generat automat din ERP.</div>
             </div>
+        </div>
         </section>
+        @endforeach
+        <div class="clearfix"></div>
     </div>
 </body>
 </html>
